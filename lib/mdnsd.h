0fab0f934d3dd0b4b99fa3c4b3fd788188750ccb
dl <corykang913@gmail.com>
2026-05-22 11:18:36 +0800

    Fix mdnsd copyright notice

--- lib/mdnsd.h
+++ lib/mdnsd.h
@@ -1,5 +1,5 @@
 /**
- *  Copyright (C) 2011-2012  Juho Vähä-Herttua
+ *  Copyright (C) 2026  kgbook
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
